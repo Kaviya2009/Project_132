620ab97ebf2b6144d1cb6f2df237347cc5137d26
Kaviya2009 <103879663+Kaviya2009@users.noreply.github.com>
2023-06-28 14:46:25 +0800

    Add files via upload

---
+++ ac.js
@@ -0,0 +1,35 @@
+status = "";
+
+function preload()
+{
+    img = loadImage('ac.jpg');
+}
+
+function setup() 
+{
+    canvas = createCanvas(380, 380);
+    canvas.center();
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Objects";
+}
+
+function modelLoaded() 
+{
+    console.log("Model Loaded!")
+    status = true;
+    objectDetector.detect(img, gotResult);
+}
+
+function gotResult(error, results) 
+{
+    if (error) 
+    {
+      console.log(error);
+    }
+    console.log(results);
+}
+
+function back()
+{
+    window.location = "index.html";
+}
---
+++ main.js
@@ -0,0 +1,24 @@
+function start()
+{
+    window.location = "ac.html";
+}
+
+function start2()
+{
+    window.location = "calc.html";
+}
+
+function start3()
+{
+    window.location = "fruits.html";
+}
+
+function start4()
+{
+    window.location = "pic.html";
+}
+
+function start5()
+{
+    window.location = "soap.html";
+}
